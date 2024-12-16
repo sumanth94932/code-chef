ea8aac4e4cb74ee562c2d0ac6a8729d71b4921a1
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-16 12:09:21 +0530

    Create 404 errors.java

---
+++ 404 errors.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s= new Scanner(System.in);
+        int X=s.nextInt();
+        if (X==404){
+            System.out.println("NOT FOUND");
+        }
+        else{
+            System.out.println("FOUND");
+        }
+	}
+}
