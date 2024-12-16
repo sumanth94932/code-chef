a2fe5ad97a2ea0c389aef2790430bd772edb9fdc
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-16 12:12:48 +0530

    Create chef and parole.java

---
+++ chef and parole.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int X=s.nextInt();
+	    if(X<7){
+	        System.out.println("No");
+	    }
+	    else{
+	        System.out.println("YES");
+	    }
+
+	}
+}
