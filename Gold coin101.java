1d5ed194de348666d6a531f1655ded4bb63f5466
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-26 12:20:34 +0530

    Create Gold coin101.java

---
+++ Gold coin101.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner (System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		if(X>Y)
+		    System.out.println(A);
+		else
+		    System.out.println(B);
+
+	}
+}
