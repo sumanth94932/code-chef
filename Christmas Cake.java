e33af3bd5211b5e55cc1f07955975e25bb2e5825
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-26 12:00:45 +0530

    Create Christmas Cake.java

---
+++ Christmas Cake.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner n=new Scanner(System.in);
+		int c;
+		int X=n.nextInt();
+		 if(X<=25)
+	    {
+	        c=25-X;
+	        System.out.println(c);
+	    }
+
+	}
+}
