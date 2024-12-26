9c2b6ea1a0a179cd59493540a54bc24cb3cccd98
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-26 12:57:06 +0530

    Create Saving Taxes.java

---
+++ Saving Taxes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t>0)
+		{
+		    int x= s.nextInt();
+		        int y = s.nextInt();
+	    	    int a=x-y;
+		        System.out.println(a);
+		   t--;
+		}
+	}
+}
